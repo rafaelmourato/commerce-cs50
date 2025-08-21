6146379353f2021e42171d0e4d28c2000cc88aef
Rafael <rafael.mourato@hotmail.com>
2025-08-21 18:28:08 -0300

    creating models

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,39 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+#user:  Listing(n:1), Watchlist(n:n), ListingsWon(1:n)
+class User(AbstractUser): #abstract has username, firs, last, password.
+    Watchlist = models.ManyToManyField('Listing', blank=True, related_name="interested_users")
 
-class User(AbstractUser):
-    pass
+
+# Listing: id, Title, Description, StartitngBid, Image(none/url), category(none:list) Active(t,f), user(1:n)(fkon.deletecascade), bid(n:1), WinningBid(1:n), Coments(n:1)
+class Listing(models.Model):
+    categories = [
+        ('Fash', 'Fashion'),
+        ('Toys', 'Toys'),
+        ('Elec', 'Electronics'),
+        ( 'Home', 'Home'),
+    ]
+    Owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    Winner = models.ForeignKey(User, on_delete=models.PROTECT, related_name="winner", null=True)
+    Title = models.CharField(max_length=64)
+    Description = models.TextField(blank=True)
+    InitialBid = models.DecimalField(max_digits = 10, decimal_places=2)
+    Image = models.URLField(max_length=500, blank=True, null=True)
+    category = models.CharField(choices=categories, null=True, blank=True, default= None)
+    Active = models.BooleanField(default=True)
+
+
+#comments: user(fkon.deletecascade), listing, content
+class Comment(models.Model):
+    Comenting = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenting")
+    OnListing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="listingComment")
+    Content = models.TextField()
+
+#Bids: User(fkon.deletecascade), Listing, Value
+class Bid(models.Model):
+    Bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bidding")
+    Receiver = models.ForeignKey(Listing, on_delete=models.PROTECT, related_name="listingBid")
+    Value = models.DecimalField(max_digits = 10, decimal_places=2)
+
+# harvard - cs50commerce
